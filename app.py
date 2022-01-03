c9bc2bca6e86a0be10ded3aa7086ebe5e02a72b9
TimotheePascal <timothee.pascal@eleves.enpc.fr>
2022-01-03 14:26:13 +0100

    Update dash_initial.py

---
+++ app.py
@@ -0,0 +1,73 @@
+import datetime
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.H1('Upload d\'une chanson'),
+    dcc.Upload(
+        id='upload-mp3',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-mp3-upload'),
+
+
+])
+
+
+def parse_contents(contents, filename, date):
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.H1('Lecture'),
+        html.Audio(src=contents, controls=True),
+        html.Hr(),
+        html.H1('Sélection d\'une partie de la chanson'),
+        dcc.RangeSlider(
+            count=1,
+            min=-5,
+            max=10,
+            step=0.5,
+            value=[-3, 7]
+        )
+    ])
+
+
+@app.callback(Output('output-mp3-upload', 'children'),
+              Input('upload-mp3', 'contents'),
+              State('upload-mp3', 'filename'),
+              State('upload-mp3', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
